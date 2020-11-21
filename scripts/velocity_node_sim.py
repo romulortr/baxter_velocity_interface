e6cc2199f4711178cb732f11c50464184c1c4d77
romulortr <romulortr@gmail.com>
2020-11-21 18:20:32 +0000

    working with gazebo, needs cleaning

--- scripts/velocity_node_sim.py
+++ scripts/velocity_node_sim.py
@@ -2,22 +2,24 @@
 import numpy as np
 import random
 import math
+from scipy.spatial.transform import Rotation as scipy
 
 import rospy
 from geometry_msgs.msg import Twist 
 from std_msgs.msg import UInt16
-
+from geometry_msgs.msg import Twist
 from baxter_core_msgs.msg import JointCommand
 
 import baxter_interface
 from baxter_interface import CHECK_VERSION
 from baxter_pykdl import baxter_kinematics
-
 class VelocityInterface:
   def __init__(self):
     self.kin = baxter_kinematics('right') 
     #
     self._pub_rate = rospy.Publisher('robot/joint_state_publish_rate', UInt16, queue_size=10)
+    rospy.Subscriber("cmd_vel", Twist, self.twist_callback)
+
     self._limb = baxter_interface.limb.Limb("right")
     self._joint_names = self._limb.joint_names()
     self.nb_joints = len(self._limb.joint_names())
@@ -25,13 +27,14 @@ class VelocityInterface:
     # control parameters
     self._control_rate = 500.0  # Hz
     self._filter_rate = 50.0  # Hz
-
+    self._velocity_rate = 25 # Hz (This should be equal or faster than the rate of twist message) 
     print("Getting robot state... ")
     self._robot_state = baxter_interface.RobotEnable(CHECK_VERSION)
     self._init_state = self._robot_state.state().enabled
     print("Enabling robot... ")
     self._robot_state.enable()
     self._pub_rate.publish(self._control_rate)
+ 
     print("Moving to neutral..")
     self.set_neutral()
     print("Ready to control in velocity... ")
@@ -40,59 +43,84 @@ class VelocityInterface:
     self.gain = .01*np.ones(self.nb_joints)
     self.integrator = np.zeros(self.nb_joints)
     self.desired_joint_velocity = 0.*np.ones(self.nb_joints)
+    self.desired_velocity = np.zeros(6)
+    self.current_velocity = np.zeros(7)
     self.direction = np.zeros(self.nb_joints)
     self.is_filter_active = False
-
+    
     # set joint state publishing to 500Hz
     self.control_timer = rospy.Timer(rospy.Duration(1./self._control_rate), self.control_callback)
     self.filter_timer = rospy.Timer(rospy.Duration(1./self._filter_rate), self.filter_callback)    
+    self.velocity_timer = rospy.Timer(rospy.Duration(1./self._velocity_rate), self.velocity_callback)    
 
   # Send linear and angular velocity (this works at image rate)
   def twist_callback(self, data):
-    pass
-        
+    self.desired_velocity = np.array([data.linear.x,
+                                      data.linear.y,
+                                      data.linear.z,
+                                      data.angular.x,
+                                      data.angular.y,
+                                      data.angular.z])
+ 
+                                  
   # Estimate linear and angular velocity of the end-effector
   def control_callback(self, event):
     # Most recent measurement
     joint_position = [self._limb.joint_angle(joint_name) for joint_name in self._joint_names]
     # Compute virtual waypoint to follow  desired joint velocity
     mean_gain = np.mean(self.gain)
-    delta = (self.desired_joint_velocity + self.integrator  ) * self.gain
+    delta = (self.desired_joint_velocity ) * self.gain + self.integrator
     # Send to robot using interface
     joint_command = {joint_name: joint_position[i] + delta[i] for i, joint_name in enumerate(self._joint_names) }
     self._limb.set_joint_positions(joint_command)
     
-    """
-    if joint_position > 1. and self.direction == 1:
-      self.direction = -1
-    elif joint_position < -1. and self.direction == -1:
-      self.direction = 1
-    delta = self.gain * self.direction 
-    joint_command = {joint_name: joint_position + delta}
-    self._limb.set_joint_positions(joint_command)
-    """
-
-  def filter_callback(self, event):
+  def filter_callback(self, event):       
+    # Estimate joint velocity
     joint_position = np.array([self._limb.joint_angle(joint_name) for joint_name in self._joint_names])
     if self.is_filter_active is True:   
       dt = (1./self._filter_rate)
-      current_velocity = (joint_position - self.previous_joint_position)/dt
+      self.current_velocity = (joint_position - self.previous_joint_position)/dt
       for i in range(self.nb_joints):
-        if self.desired_joint_velocity[i] - current_velocity[i] > 0:  
+        if self.desired_joint_velocity[i] - self.current_velocity[i] > 0:  
           self.gain[i] *= .9 
         else:
           self.gain[i] *= 1.05
-        self.integrator[i] += .01*(self.desired_joint_velocity[i] - current_velocity[i])
+        self.integrator[i] += .005*(self.desired_joint_velocity[i] - self.current_velocity[i])
         self.integrator[i] = min(max(self.integrator[i], -.1), .1)
-      print(self.integrator)
-
+        self.gain[i] = min(max(self.gain[i], .001), .1)        
     else:
       self.is_filter_active = True
     # saving for next iteration
     self.previous_joint_position = joint_position
 
+  def velocity_callback(self, event):
+    # Compute desired velocity in joint space
+    quat = self.kin.forward_position_kinematics()[3:]
+    rotation = scipy.from_quat(quat)
+    base_frame_linear_velocity = rotation.apply(self.desired_velocity[0:3], inverse=False)
+    base_frame_angular_velocity = rotation.apply(self.desired_velocity[3:], inverse=False)
+    base_frame_velocity = np.hstack([base_frame_linear_velocity, base_frame_angular_velocity])
 
-   
+    inverse_jacobian = self.kin.jacobian_pseudo_inverse()
+    desired_joint_velocity = np.matmul(inverse_jacobian, base_frame_velocity.reshape([6,1]))
+    self.desired_joint_velocity = np.array([desired_joint_velocity[0,0],
+                                            desired_joint_velocity[1,0],
+                                            desired_joint_velocity[2,0],
+                                            desired_joint_velocity[3,0],
+                                            desired_joint_velocity[4,0],
+                                            desired_joint_velocity[5,0],
+                                            desired_joint_velocity[6,0]])
+    
+    # Coompute current velocity in link coordinate sytem
+    jacobian = self.kin.jacobian()
+    base_current_velocity = np.matmul(jacobian, self.current_velocity.reshape([7,1]))
+    base_linear_velocity = np.array([base_current_velocity[0,0],base_current_velocity[1,0],base_current_velocity[2,0]])
+    base_angular_velocity = np.array([base_current_velocity[3,0],base_current_velocity[4,0],base_current_velocity[5,0]])
+    link_current_linear_velocity = rotation.apply(base_linear_velocity, inverse=True)
+    link_current_angular_velocity = rotation.apply(base_angular_velocity, inverse=True)
+    link_current_velocity = np.hstack([link_current_linear_velocity, link_current_angular_velocity])
+    print(link_current_linear_velocity)
+
   def set_neutral(self):
     """
     Sets arm back into a neutral pose.
@@ -115,6 +143,8 @@ class VelocityInterface:
     # Shutdown timer callback
     self.control_timer.shutdown()
     self.filter_timer.shutdown()    
+    self.velocity_timer.shutdown()    
+
     #return to normal
     self._reset_control_modes()
     self.set_neutral()
